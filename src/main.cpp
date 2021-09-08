0adf5ba69f5bebd4b41d89f1d55778f6814bac8f
GinMiya <ginmiya@ccs.tsukuba.ac.jp>
2021-09-08 13:02:08 +0900

    グローバル変数の削除，パラメータ構造体の追加，バイナリ出力の修正

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,43 @@
 #include <time.h>     // For using 'clock()' function
 #include <unistd.h>   // For using 'getopt()' function
 #include <string.h>   // For using 'string type'
-#include <algorithm>  // vector のソート用
-#include <functional> // vector のソート用
 #include <sys/stat.h> // For get state of directory(For using 'chmod' and so on.)
 #include <random>     // For using 'random()' function
 
 // ----------------------------------------
 // Define units
 // ----------------------------------------
-#define G 1.0
-#define M 1.0
-#define R 1.0
+#define G (1.0)
+#define M (1.0)
+#define R (1.0)
 
 // ----------------------------------------
-// Define global valiables ※本当は使わない方が良い
+// Define struct
 // ----------------------------------------
-unsigned int NPtcl = 1024;        // 粒子数
-double SoftLen   = (1.0 / 32.0);  // Softening Length
-double DT        = (1.0 / 128.0); // Time Step
-double DTDisp    = (1.0 / 8.0);   // 画面出力間隔
-double DTSnap    = (1.0 / 4.0);   // ファイル出力間隔
-double TEnd      = 5.0;           // 終了時間
-double RVir      = 0.1;           // ビリアル比
-bool BinaryFlg   = false;         // バイナリ出力有無
-char DirName[50] = "result/";     // 出力先ディレクトリ名
+struct Param {
+  unsigned int nPtcl; // 粒子数
+  double softLen;     // Softening Length
+  double dT;          // Time Step
+  double dTDisp;      // 画面出力間隔
+  double dTSnap;      // ファイル出力間隔
+  double tEnd;        // 終了時間
+  double rVir;        // ビリアル比
+  bool binaryFlg;     // バイナリ出力有無
+  char dirName[50];   // 出力先ディレクトリ名
+  void dispParams ( void ) {
+    std::cout << "==== Parameter Setting   ====" << std::endl;
+    std::cout << " * Number of Particles = "     << this->nPtcl     << std::endl;
+    std::cout << " * Softening length = "        << this->softLen   << std::endl;
+    std::cout << " * TimeStep = "                << this->dT        << std::endl;
+    std::cout << " * Time Step of Display = "    << this->dTDisp    << std::endl;
+    std::cout << " * Time Step of Fileoutput = " << this->dTSnap    << std::endl;
+    std::cout << " * Finish Time = "             << this->tEnd      << std::endl;
+    std::cout << " * Virial ratio = "            << this->rVir      << std::endl;
+    std::cout << " * Binary output = "           << this->binaryFlg << std::endl;
+    std::cout << " * Output directory = "        << this->dirName   << std::endl;
+    std::cout << "=============================" << std::endl;
+  }
+};
 
 // ----------------------------------------
 // Define functions
@@ -44,50 +57,50 @@ char DirName[50] = "result/";     // 出力先ディレクトリ名
 // =========================
 // 実行オプションの反映
 // =========================
-void optionHandling ( int argc, char * argv[] ) {
+void optionHandling ( int argc, char * argv[], Param * ParamSet ) {
   // --------------------
   // ** 変数定義
   int optionName;
-  extern int optind; // 魔法のフレーズ，気にしない
+  extern int optind;
   // --------------------
   // ** 実行オプションの内容を確認する
-  std::cout << "== Change global valiables ==" << std::endl;
+  std::cout << "===== Change Parameters =====" << std::endl;
   while ( ( optionName = getopt(argc, argv, "o:t:T:n:s:v:d:D:b") ) != -1 ) {
     switch ( optionName ) {
       case 'o':
-        strcpy(DirName, optarg);
-        std::cout << " * Output directory = " << DirName << std::endl;
+        strcpy(ParamSet->dirName, optarg);
+        std::cout << " * Output directory = " << ParamSet->dirName << std::endl;
         break;
       case 't':
-        DT = atof(optarg); // String型をdoubleにする
-        std::cout << " * TimeStep = " << DT << std::endl;
+        ParamSet->dT = atof(optarg); // String型をdoubleにする
+        std::cout << " * TimeStep = " << ParamSet->dT << std::endl;
         break;
       case 'T':
-        TEnd = atof(optarg); // String型をdoubleにする
-        std::cout << " * Time to end = " << TEnd << std::endl;
+        ParamSet->tEnd = atof(optarg); // String型をdoubleにする
+        std::cout << " * Finish Time = " << ParamSet->tEnd << std::endl;
         break;
       case 'n':
-        NPtcl = atoi(optarg); // String型をintにする
-        std::cout << " * Number of Particles = " << NPtcl << std::endl;
+        ParamSet->nPtcl = atoi(optarg); // String型をintにする
+        std::cout << " * Number of Particles = " << ParamSet->nPtcl << std::endl;
         break;
       case 's':
-        SoftLen = atof(optarg); // String型をdoubleにする
-        std::cout << " * Softening length = " << SoftLen << std::endl;
+        ParamSet->softLen = atof(optarg); // String型をdoubleにする
+        std::cout << " * Softening length = " << ParamSet->softLen << std::endl;
         break;
       case 'v':
-        RVir = atof(optarg); // String型をdoubleにする
-        std::cout << " * Virial ratio = " << RVir << std::endl;
+        ParamSet->rVir = atof(optarg); // String型をdoubleにする
+        std::cout << " * Virial ratio = " << ParamSet->rVir << std::endl;
         break;
       case 'd':
-        DTSnap = atof(optarg); // String型をdoubleにする
-        std::cout << " * Time Step of Fileoutput = " << DTSnap << std::endl;
+        ParamSet->dTSnap = atof(optarg); // String型をdoubleにする
+        std::cout << " * Time Step of Fileoutput = " << ParamSet->dTSnap << std::endl;
         break;
       case 'D':
-        DTDisp = atof(optarg); // String型をdoubleにする
-        std::cout << " * Time Step of Display = " << DTDisp << std::endl;
+        ParamSet->dTDisp = atof(optarg); // String型をdoubleにする
+        std::cout << " * Time Step of Display = " << ParamSet->dTDisp << std::endl;
         break;
       case 'b':
-        BinaryFlg = true;
+        ParamSet->binaryFlg = true;
         std::cout << " * Binary output is true. " << std::endl;
         break;
       default:
@@ -116,17 +129,17 @@ void makeDirectory ( char * Name ) {
 // =========================
 // ポテンシャルエネルギー計算
 // =========================
-double calcPotEne ( double m[], double x[][3] ) {
+double calcPotEne ( double m[], double x[][3], Param * ParamSet ) {
   double potEne = 0.0;
   double xij[3];
   double r2ij = 0.0;
-  for ( int i = 0; i < NPtcl - 1; ++i ) {
-    for ( int j = i + 1; j < NPtcl; ++j ) {
+  for ( int i = 0; i < ParamSet->nPtcl - 1; ++i ) {
+    for ( int j = i + 1; j < ParamSet->nPtcl; ++j ) {
       xij[0] = x[j][0] - x[i][0];
       xij[1] = x[j][1] - x[i][1];
       xij[2] = x[j][2] - x[i][2];
       r2ij = (xij[0] * xij[0]) + (xij[1] * xij[1]) + (xij[2] * xij[2]);
-      potEne += - (m[i] * m[j]) / sqrt(r2ij + (SoftLen * SoftLen));
+      potEne += - (m[i] * m[j]) / sqrt(r2ij + (ParamSet->softLen * ParamSet->softLen));
     }
   }
   return potEne;
@@ -134,9 +147,9 @@ double calcPotEne ( double m[], double x[][3] ) {
 // =========================
 // 運動エネルギー計算
 // =========================
-double calcKinEne ( double m[], double v[][3] ) {
+double calcKinEne ( double m[], double v[][3], Param * ParamSet ) {
   double kinEne = 0;
-  for ( int i = 0; i < NPtcl; ++i ) {
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
     kinEne += 0.5 * m[i]
               * ((v[i][0] * v[i][0]) + (v[i][1] * v[i][1]) + (v[i][2] * v[i][2]));
   }
@@ -162,16 +175,16 @@ double gaussian ( void ) {
 // =========================
 // 初期分布作成
 // =========================
-void makePtclDistribution ( double m[], double x[][3], double v[][3] ) {
+void makePtclDistribution ( double m[], double x[][3], double v[][3], Param * ParamSet ) {
   // --------------------
   // ** 粒子の質量を設定
-  for ( int i = 0; i < NPtcl; ++i ) {
-    m[i] = (double)M / NPtcl;
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
+    m[i] = (double)M / ParamSet->nPtcl;
   }
   // --------------------
   // ** 粒子の位置を設定
   double r2 = 0.0;
-  for ( int i = 0; i < NPtcl; ++i ) {
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
     do {
       r2 = 0.0;
       for ( int k = 0; k < 3; ++k ) {
@@ -182,11 +195,11 @@ void makePtclDistribution ( double m[], double x[][3], double v[][3] ) {
   }
   // --------------------
   // ** ポテンシャル計算と速度分散の計算
-  double potEne = calcPotEne(m, x);
-  double sigma  = sqrt( (2.0 * RVir * fabs(potEne)) / (3.0 * M) );
+  double potEne = calcPotEne(m, x, ParamSet);
+  double sigma  = sqrt( (2.0 * ParamSet->rVir * fabs(potEne)) / (3.0 * M) );
   // --------------------
   // ** 速度分散からマクスウェル分布で速度を与える
-  for ( int i = 0; i < NPtcl; ++i ) {
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
     for ( int k = 0; k < 3; ++k ) {
       v[i][k] = sigma * gaussian();
     }
@@ -195,10 +208,10 @@ void makePtclDistribution ( double m[], double x[][3], double v[][3] ) {
 // =========================
 // 重力計算
 // =========================
-void calcForce ( double m[], double x[][3], double a[][3] ) {
+void calcForce ( double m[], double x[][3], double a[][3], Param * ParamSet ) {
   // --------------------
   // ** 加速度の初期化
-  for ( int i = 0; i < NPtcl; ++i ) {
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
     a[i][0] = 0.0;
     a[i][1] = 0.0;
     a[i][2] = 0.0;
@@ -209,8 +222,8 @@ void calcForce ( double m[], double x[][3], double a[][3] ) {
   double r3inv = 0.0;
   // --------------------
   // ** 重力相互作用を計算する（直接計算法）
-  for ( int i = 0; i < NPtcl - 1; ++i ) {
-    for ( int j = i + 1; j < NPtcl; ++j ) {
+  for ( int i = 0; i < ParamSet->nPtcl - 1; ++i ) {
+    for ( int j = i + 1; j < ParamSet->nPtcl; ++j ) {
       // --------------------
       // ** 相対距離の計算
       xij[0] = x[j][0] - x[i][0];
@@ -218,7 +231,7 @@ void calcForce ( double m[], double x[][3], double a[][3] ) {
       xij[2] = x[j][2] - x[i][2];
       // --------------------
       // ** 粒子間距離の3乗の逆数を計算
-      r3inv = 1.0 / pow(sqrt((xij[0] * xij[0]) + (xij[1] * xij[1]) + (xij[2] * xij[2]) + (SoftLen * SoftLen)), 3.0);
+      r3inv = 1.0 / pow(sqrt((xij[0] * xij[0]) + (xij[1] * xij[1]) + (xij[2] * xij[2]) + (ParamSet->softLen * ParamSet->softLen)), 3.0);
       // --------------------
       // ** j粒子からi粒子への作用計算
       a[i][0] += G * m[j] * xij[0] * r3inv;
@@ -235,60 +248,56 @@ void calcForce ( double m[], double x[][3], double a[][3] ) {
 // =========================
 // 速度を半ステップ進める
 // =========================
-void kickTime ( double a[][3], double v[][3] ) {
-  for ( int i = 0; i < NPtcl; ++i ) {
-    v[i][0] += 0.5 * a[i][0] * DT;
-    v[i][1] += 0.5 * a[i][1] * DT;
-    v[i][2] += 0.5 * a[i][2] * DT;
+void kickTime ( double a[][3], double v[][3], Param * ParamSet ) {
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
+    v[i][0] += 0.5 * a[i][0] * ParamSet->dT;
+    v[i][1] += 0.5 * a[i][1] * ParamSet->dT;
+    v[i][2] += 0.5 * a[i][2] * ParamSet->dT;
   }
 }
 // =========================
 // 位置を１ステップ進める
 // =========================
-void driftTime ( double x[][3], double v[][3] ) {
-  for ( int i = 0; i < NPtcl; ++i ) {
-    x[i][0] += v[i][0] * DT;
-    x[i][1] += v[i][1] * DT;
-    x[i][2] += v[i][2] * DT;
+void driftTime ( double x[][3], double v[][3], Param * ParamSet ) {
+  for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
+    x[i][0] += v[i][0] * ParamSet->dT;
+    x[i][1] += v[i][1] * ParamSet->dT;
+    x[i][2] += v[i][2] * ParamSet->dT;
   }
 }
 // =========================
 // 粒子情報を出力する
 // =========================
-void outputPtclInfo ( double m[], double x[][3], double v[][3], int ID ) {
+void outputPtclInfo ( double m[], double x[][3], double v[][3], int ID, Param * ParamSet ) {
   // --------------------
   // ** 変数定義
   char filePath[50];
-  sprintf(filePath, "%s/%d.dat", DirName, ID);
+  sprintf(filePath, "%s/%d.dat", ParamSet->dirName, ID);
   std::ofstream ofsPtcl; // Energyに関する情報出力
   // --------------------
   // ** Binary出力/Ascii出力
-  if ( BinaryFlg ) {
+  if ( ParamSet->binaryFlg ) {
     ofsPtcl.open(filePath, std::ios::binary);
     // --------------------
     // ** 書き込み用 struct
     struct ptcl {
-      float mass;
       float pos[3];
       float vel[3];
-      int   idx;
     };
     // --------------------
     // ** 書き込み用のstructを作る
     struct ptcl tmp;
     // --------------------
     // ** 全粒子の情報出力
-    for ( int i = 0; i < NPtcl; ++i ) {
+    for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
       // --------------------
       // ** 書き込み用 struct に情報をコピー
-      tmp.mass   = m[i];
       tmp.pos[0] = x[i][0];
       tmp.pos[1] = x[i][1];
       tmp.pos[2] = x[i][2];
       tmp.vel[0] = v[i][0];
       tmp.vel[1] = v[i][1];
       tmp.vel[2] = v[i][2];
-      tmp.idx    = i;
       // ----------------------------------------
       // ** 粒子情報の書き込み
       ofsPtcl.write((char *)&tmp, sizeof(ptcl));
@@ -299,12 +308,12 @@ void outputPtclInfo ( double m[], double x[][3], double v[][3], int ID ) {
     ofsPtcl.open(filePath);
     // --------------------
     // ** 全粒子の情報出力
-    for ( int i = 0; i < NPtcl; ++i ) {
-      ofsPtcl << x[i][0] << ","
-              << x[i][1] << ","
-              << x[i][2] << ","
-              << v[i][0] << ","
-              << v[i][1] << ","
+    for ( int i = 0; i < ParamSet->nPtcl; ++i ) {
+      ofsPtcl << x[i][0] << " "
+              << x[i][1] << " "
+              << x[i][2] << " "
+              << v[i][0] << " "
+              << v[i][1] << " "
               << v[i][2] << std::endl;
     }
   }
@@ -317,18 +326,30 @@ void outputPtclInfo ( double m[], double x[][3], double v[][3], int ID ) {
 // ----------------------------------------
 int main ( int argc, char * argv[] ) {
   // --------------------
-  // ** 実行オプションがある場合に初期設定を行う
+  // ** シミュレーションの設定パラメータのデフォルト値をセットする　（今回はスタック領域に確保する）
+  struct Param ParamSet;
+  ParamSet.nPtcl     = 1024;           // 粒子数
+  ParamSet.softLen   = 1.0 / 32.0;     // Softening Length
+  ParamSet.dT        = 1.0 / 128.0;    // Time Step
+  ParamSet.dTDisp    = 1.0 / 8.0;      // 画面出力間隔
+  ParamSet.dTSnap    = 1.0 / 4.0;      // ファイル出力間隔
+  ParamSet.tEnd      = 5.0;            // 終了時間
+  ParamSet.rVir      = 0.1;            // ビリアル比
+  ParamSet.binaryFlg = false;          // バイナリ出力有無
+  strcpy(ParamSet.dirName, "result/"); // 出力先ディレクトリ名
+  // --------------------
+  // ** 実行オプションがある場合に設定パラメータの変更を行う
   if ( argc != 1 ) {
-    optionHandling(argc, argv);
+    optionHandling(argc, argv, &ParamSet);
   }
   // --------------------
-  // ** 結果保存用のディレクトリを作る
-  makeDirectory(DirName);
+  // ** パラメータ一覧を出力
+  ParamSet.dispParams();
   // --------------------
   // ** 粒子の変数定義
-  double m[NPtcl]; // 質量
-  double x[NPtcl][3], v[NPtcl][3], a[NPtcl][3]; // 位置，速度，加速度
-  double vhalf[NPtcl][3]; // Leap-Frog法用の半ステップ進んだ速度
+  double m[ParamSet.nPtcl]; // 質量
+  double x[ParamSet.nPtcl][3], v[ParamSet.nPtcl][3], a[ParamSet.nPtcl][3]; // 位置，速度，加速度
+  double vhalf[ParamSet.nPtcl][3]; // Leap-Frog法用の半ステップ進んだ速度
   // --------------------
   // ** 系の変数定義
   double tSys, tSnap, tDisp = 0.0; // 系の経過時間，スナップ時間，画面表示時間
@@ -338,61 +359,63 @@ int main ( int argc, char * argv[] ) {
   // --------------------
   // ** ファイル出力用変数定義 ofstream
   char enePath[50];
-  sprintf(enePath, "%s/t-de.dat", DirName);
+  sprintf(enePath, "%s/t-de.dat", ParamSet.dirName);
   std::ofstream ofsEne; // Energyに関する情報出力
   // --------------------
   // ** 初期分布を作る
-  srand(1223L);
-  makePtclDistribution(m, x, v);
+  makePtclDistribution(m, x, v, &ParamSet);
   // --------------------
   // ** 初期エネルギーの計算
-  pot = calcPotEne(m, x);
-  kin = calcKinEne(m, v);
+  pot = calcPotEne(m, x, &ParamSet);
+  kin = calcKinEne(m, v, &ParamSet);
   eTot = eInit = pot + kin;
   // --------------------
   // ** 初期加速度の計算
-  calcForce(m, x, a);
+  calcForce(m, x, a, &ParamSet);
+  // --------------------
+  // ** 結果保存用のディレクトリを作る
+  makeDirectory(ParamSet.dirName);
   // --------------------
   // ** 時間積分前の初期状態アウトプット
-  outputPtclInfo(m, x, v, nStep);
+  outputPtclInfo(m, x, v, nStep, &ParamSet);
   // --------------------
   // ** エネルギー情報の出力
   ofsEne.open(enePath); // ファイルを開けっ放しにする
   // --------------------
   // ** 時間積分の開始
-  while ( tSys < TEnd ) {
+  while ( tSys < ParamSet.tEnd ) {
     // --------------------
     // ** 粒子情報スナップ
-    if ( (tSys >= tSnap) || ( (tSys + DT) - tSnap ) > (tSnap - tSys) ) {
+    if ( (tSys >= tSnap) || ( (tSys + ParamSet.dT) - tSnap ) > (tSnap - tSys) ) {
       // --------------------
       // ** エネルギーと粒子状態の出力
       ofsEne << tSys << "," << pot << "," << kin << "," << (eTot - eInit) / eInit <<  std::endl;
-      outputPtclInfo(m, x, v, nStep);
-      tSnap += DTSnap;
+      outputPtclInfo(m, x, v, nStep, &ParamSet);
+      tSnap += ParamSet.dTSnap;
     }
     // --------------------
     // ** エネルギー計算
-    pot = calcPotEne(m, x);
-    kin = calcKinEne(m, v);
+    pot = calcPotEne(m, x, &ParamSet);
+    kin = calcKinEne(m, v, &ParamSet);
     eTot = pot + kin;
     // --------------------
     // ** 画面出力
-    if ( (tSys >= tDisp) || ( (tSys + DT) - tDisp ) > (tDisp - tSys) ) {
+    if ( (tSys >= tDisp) || ( (tSys + ParamSet.dT) - tDisp ) > (tDisp - tSys) ) {
       std::cout << "time: " << tSys << "  energy error: " << (eTot - eInit) / eInit << std::endl;
-      tDisp += DTDisp;
+      tDisp += ParamSet.dTDisp;
     }
     // --------------------
     // ** Leap-Frog
-    kickTime(a, v);
-    tSys += DT;
+    kickTime(a, v, &ParamSet);
+    tSys += ParamSet.dT;
     ++nStep;
-    driftTime(x, v);
+    driftTime(x, v, &ParamSet);
     // --------------------
     // ** 重力計算
-    calcForce(m, x, a);
+    calcForce(m, x, a, &ParamSet);
     // --------------------
     // ** Leap-Frog
-    kickTime(a, v);
+    kickTime(a, v, &ParamSet);
   }
   // --------------------
   // ** エネルギー出力
